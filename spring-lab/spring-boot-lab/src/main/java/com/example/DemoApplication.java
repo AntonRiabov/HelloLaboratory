3dd39c1aa8fc441724cc5cf29eee40af3892ad4a
Anton Riabov <anton.riabov@engagepoint.com>
2016-08-12 18:28:50 +0300

    add mvc component example, health indicator,  resource processor

--- spring-lab/spring-boot-lab/src/main/java/com/example/DemoApplication.java
+++ spring-lab/spring-boot-lab/src/main/java/com/example/DemoApplication.java
@@ -3,11 +3,19 @@ package com.example;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.actuate.health.Health;
+import org.springframework.boot.actuate.health.HealthIndicator;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.repository.query.Param;
 import org.springframework.data.rest.core.annotation.RepositoryRestResource;
+import org.springframework.hateoas.Link;
+import org.springframework.hateoas.Resource;
+import org.springframework.hateoas.ResourceProcessor;
+import org.springframework.stereotype.Component;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -16,6 +24,7 @@ import javax.persistence.GeneratedValue;
 import javax.persistence.Id;
 import java.util.Arrays;
 import java.util.Collection;
+import java.util.List;
 
 @SpringBootApplication
 public class DemoApplication {
@@ -25,7 +34,13 @@ public class DemoApplication {
     }
 
     @Bean
+    HealthIndicator healthIndicator(){
+        return ()-> Health.status("I <3 SFJUG!").build();
+    }
+
+    @Bean
     CommandLineRunner runner(ReservationRepository rr) {
+
         return args -> {
             Arrays.asList("Les,Josh,Phil,Saasha,Peter".split(",")).forEach(n -> rr.save(new Reservation(n)));
             rr.findAll().forEach(System.out::println);
@@ -53,6 +68,32 @@ public class DemoApplication {
 //    private ReservationRepository reservationRepository;
 //}
 
+
+@Controller
+class ReservationMvcController{
+    @RequestMapping("/reservations.php")
+    String reservations(Model model){
+         model.addAttribute("reservations", this.reservationRepository.findAll());
+
+//         return "reservations"; // src/main/resources/templates/ + $X + .html
+         return "foo/reservations"; // src/main/resources/foo/templates/ + $X + .html
+    }
+
+    @Autowired
+    private ReservationRepository reservationRepository;
+}
+
+@Component
+class ReservationresourceProcessor implements ResourceProcessor<Resource<Reservation>>{
+
+    @Override
+    public Resource<Reservation> process(Resource<Reservation> reservationResource) {
+        reservationResource.add(new Link("http://s3.com/imgs/" + reservationResource.getContent().getId() + ".jpg", "profile-photo") {
+        });
+
+        return reservationResource;
+    }
+}
 
 @RepositoryRestResource
 interface ReservationRepository extends JpaRepository<Reservation, Long> {
