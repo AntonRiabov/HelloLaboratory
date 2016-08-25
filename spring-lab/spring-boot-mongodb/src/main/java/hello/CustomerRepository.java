2ce6e113272d167c71a1818c06e4d06ff4c4aed0
AntonRiabov <madeofsolar@gmail.com>
2016-08-25 09:16:18 +0300

    create simple rest controller for managing repository

---
+++ spring-lab/spring-boot-mongodb/src/main/java/hello/CustomerRepository.java
@@ -0,0 +1,16 @@
+package hello;
+
+import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.data.rest.core.annotation.RepositoryRestResource;
+
+import java.util.List;
+
+/**
+ * Created by paladii on 24.08.2016.
+ */
+@RepositoryRestResource(collectionResourceRel = "customers", path = "customers")
+public interface CustomerRepository extends MongoRepository<Customer, String> {
+
+    Customer findByFirstName(String firstName);
+    List<Customer> findByLastName(String lastName);
+}
